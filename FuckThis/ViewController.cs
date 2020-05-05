feb7932593ca7ad988e52bb2f24a183a9788d28b
Greg Rell <gregory.rell@universalorlando.com>
2020-05-05 12:56:25 -0400

    Initial check-in of module FuckThis

---
+++ FuckThis/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace FuckThis
+{
+    public partial class ViewController : NSViewController
+    {
+        private int numberPfTimesClicked = 0;
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+            ClickedLabel1.StringValue = "Click it";
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void ClickedButton(NSObject sender)
+        {
+            ClickedLabel1.StringValue = string.Format("Fuck Mac {0} times", ++numberPfTimesClicked, numberPfTimesClicked);
+        }
+    }
+}
---
+++ FuckThis/ViewController.designer.cs
@@ -0,0 +1,29 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace FuckThis
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField ClickedLabel1 { get; set; }
+
+		[Action ("ClickedButton:")]
+		partial void ClickedButton (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ClickedLabel1 != null) {
+				ClickedLabel1.Dispose ();
+				ClickedLabel1 = null;
+			}
+		}
+	}
+}
