68daf7f5147dc6fba7d4918fd10e5376cac965e6
Greg Rell <gregory.rell@universalorlando.com>
2020-05-11 12:12:24 -0400

    Multiple windows opening and working. ID had to be filled in inside xcode

--- FuckThis/AppDelegate.cs
+++ FuckThis/AppDelegate.cs
@@ -6,6 +6,22 @@ namespace FuckThis
     [Register("AppDelegate")]
     public partial class AppDelegate : NSApplicationDelegate
     {
+        public int UntitledWindowCount { get; set; } = 1;
+
+        [Export("newDocument:")]
+        void NewDocument(NSObject sender)
+        {
+            // Get new window
+            var storyboard = NSStoryboard.FromName("Main", null);
+            var controller = storyboard.InstantiateControllerWithIdentifier("MainWindow") as NSWindowController;
+
+            // Display
+            controller.ShowWindow(this);
+
+            // Set the title
+            controller.Window.Title = (++UntitledWindowCount == 1) ? "untitled" : string.Format("untitled {0}", UntitledWindowCount);
+        }
+
         public AppDelegate()
         {
         }
